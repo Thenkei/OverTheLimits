de1f14697ef5aff95988bdf7c0a95c5585fcd393
Thendeo <alexislafont.43@gmail.com>
2019-03-19 00:06:52 +0100

    Added basic intel to dummy bot : random card

--- dummy.js
+++ dummy.js
@@ -1,6 +1,13 @@
 const openSocket = require('socket.io-client');
 const socket = openSocket(`http://localhost:3000`, { path: '/api/socket.io' });
 
+const CHANNEL_STATUS = {
+  IDLE: 'IDLE',
+  WAITING_GAME: 'WAITING_GAME',
+  PLAYING_CARD: 'PLAYING_CARD',
+  JUDGING_CARD: 'JUDGING_CARD',
+};
+
 function addListener(name, cb) {
   if (!socket.hasListeners(name)) {
     socket.on(name, cb);
@@ -40,6 +47,9 @@ function success(cb) {
   addListener('success', successMsg => cb(successMsg));
 }
 
+function sendMessage(msg) {
+  socket.emit('chat/message', msg);
+}
 
 // CODE HERE
 
@@ -54,10 +64,36 @@ if(args.length === 0) {
 
 function updateBot(inChannel) {
   party = inChannel;
-  bot = inChannel.players.find(p => bot.id === bot.id);
+  bot = inChannel.players.find(p => p.id === bot.id);
   console.log('[Dummy] Updated channel');
-
   //HERE BOT CODE check party.currentStatus for UTLGame to know what to do
+  switch(party.currentStatus)
+  {
+    //
+    case CHANNEL_STATUS.IDLE:
+    break;
+    case CHANNEL_STATUS.WAITING_GAME:
+      sendMessage("Salut les mecs ! On commence la partie ?");
+    break;
+    case CHANNEL_STATUS.PLAYING_CARD:
+
+    console.log(bot.isGameMaster);
+      if(!bot.isGameMaster)
+      {
+        if(bot.answers.length === 0)
+        {
+          selectedAnswers(Math.floor((Math.random() * 10) + 1));
+          sendMessage("Ok pour moi");
+        }
+      }
+      // Is the bot already played ?
+    break;
+    case CHANNEL_STATUS.JUDGING_CARD:
+    // Is the bot master ?
+    //sendMessage("Hé bah ... Vous êtes trash les mecs");
+
+    break;
+  }
 }
 
 createPlayer('DUMMY_BOT', (player) => { bot = player });
